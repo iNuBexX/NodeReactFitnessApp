31d67417cc9c694cf1408d78a6af24d6a5c6e714
iNuBexX <kelkor664455@gmail.com>
2024-03-06 14:33:23 +0100

    form now can signup

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.post('/login', (req, res) => {
     if (!username || !password) {
         return res.status(400).json({ error: 'Username and password are required.' });
     }
-
+    
     const query = 'SELECT * FROM users WHERE username = $1 AND password = $2';
     client.query(query, [username, password])
         .then((result) => {
